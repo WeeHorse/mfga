f19fd6b5e4cbcf5f6b47a55b2509fcaf9ddc0648
WeeHorse <benjaminberglund@gmail.com>
2023-04-24 13:44:48 +0200

    fixed react example to handle undefined props

--- examples/react/src/pages/FormExample.jsx
+++ examples/react/src/pages/FormExample.jsx
@@ -17,24 +17,24 @@ export default function () {
     return <form id="form1" onSubmit={submitCheckout} onChange={watchFormState}>
 
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" defaultValue={data.email}/>
+        <input type="email" name="email" defaultValue={data?.email}/>
 
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" defaultValue={data.password}/>
+        <input type="password" name="password" defaultValue={data?.password}/>
 
         <label htmlFor="street_address">Address</label>
-        <input type="text" name="street_address" defaultValue={data.street_address} placeholder="1234 Main St" />       
+        <input type="text" name="street_address" defaultValue={data?.street_address} placeholder="1234 Main St" />       
 
         <label htmlFor="states">States</label>
         <select name="states" multiple>
-            {data.states.map(state=> <option value={state.value}>{state.text}</option>)}
+            {data?.states.map(state=> <option key={state.value} value={state.value}>{state.text}</option>)}
         </select>
 
         <label htmlFor="city">City</label>
-        <input type="text" name="city" defaultValue={data.city} />
+        <input type="text" name="city" defaultValue={data?.city} />
 
         <label htmlFor="zip">Zip</label>
-        <input type="text" name="zip" defaultValue={data.zip} />
+        <input type="text" name="zip" defaultValue={data?.zip} />
         
         <input type="submit" disabled value="Send"/>
     </form>
