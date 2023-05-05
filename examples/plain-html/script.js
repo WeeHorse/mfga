0259b267ce31f152fd4f4e0bb21176c5dba3013e
WeeHorse <benjaminberglund@gmail.com>
2023-05-05 18:04:59 +0200

    wip

--- examples/plain-html/script.js
+++ examples/plain-html/script.js
@@ -1,33 +1,4 @@
 import { watchFormState, handleSubmit } from '../../index.js'
-
-const form1 = document.querySelector('#form1') // select the form to handle with MFG(a)
-
-// if you are using the form state watcher, you will probably want to disable the submit button until there is a change in data:
-form1.querySelector('*[type="submit"]').disabled = true // (alternatively, you can do this in your html, the form2 example does that)
-
-form1.onsubmit = handleSubmit // connects MFG(a) submit handler to form submit event
-form1.onchange = watchFormState // connects MFG(a) form state handler to form change event
-
-
-// Another form?
-
-const form2 = document.querySelector('#form2') // select another form to handle with MFG(a), if you have more than one
-
-/* 
-Apply (optional) data (state) here, like for an update (put). 
-Typically this would be a data object from fetch or similar.
-If there is no previous data, like for a create (post), 
-there is no need to call setInitialState 
-*/
-// form2.initialState = {
-//   name: "No name",
-//   slogan: "No slogan"
-// }
-
-form2.onsubmit = handleSubmit // set MFG(a) submit handler to form submit event
-form2.onchange = watchFormState // set MFG(a) form state handler to form change event
-
-// just like you set the event handlers in the code, above, you can add method and action the same way too, if you prefer:
-form2.method = "put" 
-form2.action = "https://1589dbb9-350b-40d2-9f8f-dd9df788b78f.mock.pstmn.io/update"
+window.handleSubmit = handleSubmit // expose MFG(a) submit handler to HTML-scope
+window.watchFormState = watchFormState // expose MFG(a) form state handler to HTML-scope
 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 // Handle Submit
 export async function handleSubmit(e){
     const form = e.target
     e.preventDefault()   
     form.classList.add('mfga-processing')
+    
     const result = await fetch(form.action,{
         method : form.attributes.method.value, // bypassing e.target.method since it only allows post, get and dialog: https://www.w3.org/TR/html401/interact/forms.html#h-17.13
         headers: {
@@ -19,44 +21,31 @@ export async function handleSubmit(e){
 }
 
 // Handle Form State
-// export const dirtyFields = new Set()
-
-// let initialState = {}
-
-// export function setInitialState(state){
-//     if(state){
-//         initialState = state 
-//     }
-// }
-
 export function watchFormState (e){
-    const form = e.target.closest('form')
-    if(form.initialState === undefined){
-        form.initialState = {}
-    }
+    const field = e.target
+    const form = field.closest('form')
     if(form.dirtyFields === undefined){
         form.dirtyFields = new Set()
     }
-    (form.initialState[e.target.name] != e.target.value) ? form.dirtyFields.add(e.target.name) : form.dirtyFields.delete(e.target.name)
+    if(fieldState(field).changed){
+        form.dirtyFields.add(field.name)
+    }else{
+        form.dirtyFields.delete(field.name)
+    }
+    console.log(fieldState(field))
     console.log(form.dirtyFields)
-    form.querySelector('*[type="submit"]').disabled = (form.dirtyFields.size === 0)        
+    form.dirty = (form.dirtyFields.size === 0) 
+    form.querySelector('*[type="submit"]').disabled = form.dirty        
     preventDirtyNavigation(form)    
 }
 
 
 // Handle Modified Form
-const dirtyNavDisabled = false
-let dirtyNavInited = false
-
-// export function disableDirtyNavigation(){
-//     dirtyNavDisabled = false
-// }
-
 function preventDirtyNavigation(form){
     if(form.dirtyNavInited || form.dirtyNavDisabled) return
     form.dirtyNavInited = true
     form.dirtyNavigationListener = e => {
-        if(dirtyFields.size !== 0){
+        if(form.dirty){
             e.preventDefault();
             e.returnValue = '';
         }
@@ -64,58 +53,79 @@ function preventDirtyNavigation(form){
     window.addEventListener('beforeunload', form.dirtyNavigationListener)
 }
 
-// function dirtyNavigationListener(event){
-//     if(dirtyFields.size !== 0){
-//         event.preventDefault();
-//         event.returnValue = '';
-//     }
-// }
 
-// Objectify Form
-export function objectifyForm(form){
-    const obj = {}
-    for(let elem of form.elements){
-        switch(elem.type){
-            case 'radio':
-                if(!obj[elem.name]){
-                    obj[elem.name] = elem.checked? elem.value : null
-                }                
-            break;
-            case 'checkbox':
-                if(form[elem.name].length){
-                    if(!obj[elem.name]?.push){
-                        obj[elem.name] = []
-                    }
-                    if(elem.checked){
-                        obj[elem.name].push(elem.value)
-                    }
-                }else{
-                    if(elem.checked){
-                        obj[elem.name] = elem.value
-                    } 
+// Standardize field data representation (helper)
+function fieldState(field){
+    let changed = false
+    let value = null
+    switch(field.type){
+        case 'radio':
+            return {
+                changed: field.defaultChecked !== field.checked,
+                value: field.checked? field.value : null
+            }
+        break;
+        case 'checkbox':
+            const isMultipleFieldsArray = field.closest('form')[field.name].length
+            if(isMultipleFieldsArray){
+                value = field.checked? field.value : null 
+            }else{
+                value = field.checked? (field.value === "true"? true : field.value) : (field.value === "true"? false : null)
+            }
+            return {
+                changed: field.defaultChecked !== field.checked,
+                value,
+                isMultipleFieldsArray
+            }
+        break;
+        case 'select-multiple':
+            const values = []
+            for(let option of field.options){
+                if(option.defaultSelected !== option.selected) changed = true
+                if(option.selected) values.push(option.value)
+            }            
+            return {
+                changed,
+                value: values
+            }
+        break;
+        case 'select-one':
+            for(let option of field.options){
+                if(option.defaultSelected !== option.selected){
+                    changed = true
                 }
-            break;
-            case 'select-multiple':
-                if(!obj[elem.name]?.push){
-                    obj[elem.name] = []
-                }
-                for(let option of elem.options){
-                    if(option.selected){
-                        obj[elem.name].push(option.value)
-                    }
-                }
-            break;
-            case 'submit':
-                // don't capture it
-            break;
-            case 'fieldset':
-                // don't capture it
-            break;
-            default:
-                obj[elem.name] = elem.value? elem.value : null
-            break;
-        }        
+            }
+            return {
+                changed,
+                value: field.value
+            }
+        break;
+        default:
+            return {
+                changed: field.defaultValue !== field.value,
+                value: field.value
+            }
+        break;
     }
+}
 
-    return obj
+
+// Convert form fields to payload object 
+export function objectifyForm(form){
+    const body = {}
+    for(let field of form.elements){
+        if(['submit', 'fieldset'].includes(field.type)) continue;
+        const state = fieldState(field)
+        if(state.isMultipleFieldsArray){
+            if(!body[field.name]){
+                body[field.name] = []
+            }
+            if(state.value){
+                body[field.name].push(state.value)
+            }
+        }else{
+            body[field.name] = state.value
+        } 
+    }
+    return body
 }
