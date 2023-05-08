367a3553526d31e980291367e9ec134bdbccc7c8
WeeHorse <benjaminberglund@gmail.com>
2023-05-08 08:53:17 +0200

    v2 api handles multiple forms and all kinds of form fields

--- index.js
+++ index.js
@@ -1,105 +1,121 @@
+
 // Handle Submit
 export async function handleSubmit(e){
+    const form = e.target
     e.preventDefault()   
-    e.target.classList.add('mfga-processing')
-    const result = await fetch(e.target.action,{
-        method : e.target.attributes.method.value, // bypassing e.target.method since it only allows post, get and dialog: https://www.w3.org/TR/html401/interact/forms.html#h-17.13
+    form.classList.add('mfga-processing')    
+    const result = await fetch(form.action,{
+        method : form.attributes.method.value, // bypassing e.target.method since it only allows post, get and dialog: https://www.w3.org/TR/html401/interact/forms.html#h-17.13
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(objectifyForm(e.target))            
+        body: JSON.stringify(objectifyForm(form))            
     })
-    e.target.classList.remove('mfga-processing')
+    form.classList.remove('mfga-processing')
     if(result.ok){
-        e.target.querySelector('*[type="submit"]').disabled = true
-        window.removeEventListener('beforeunload', dirtyNavigationListener)
+        form.querySelector('*[type="submit"]').disabled = true
+        window.removeEventListener('beforeunload', form.dirtyNavigationListener)
         return result
     }
 }
 
 // Handle Form State
-export const dirtyFields = new Set()
-let initialState = {}
-
-export function setInitialState(state){
-    if(state){
-        initialState = state 
-    }
-}
-
 export function watchFormState (e){
-    (initialState[e.target.name] != e.target.value) ? dirtyFields.add(e.target.name) : dirtyFields.delete(e.target.name)
-    e.target.closest('form').querySelector('*[type="submit"]').disabled = (dirtyFields.size === 0)        
-    preventDirtyNavigation()
+    const field = e.target
+    const form = field.closest('form')
+    if(form.dirtyFields === undefined){
+        form.dirtyFields = new Set()
+    }
+    if(fieldState(field).changed){
+        form.dirtyFields.add(field.name)
+    }else{
+        form.dirtyFields.delete(field.name)
+    }
+    form.dirty = (form.dirtyFields.size === 0) 
+    form.querySelector('*[type="submit"]').disabled = form.dirty        
+    preventDirtyNavigation(form)    
 }
 
 
 // Handle Modified Form
-const dirtyNavDisabled = false
-let dirtyNavInited = false
-
-export function disableDirtyNavigation(){
-    dirtyNavDisabled = false
+function preventDirtyNavigation(form){
+    if(form.dirtyNavInited || form.dirtyNavDisabled) return
+    form.dirtyNavInited = true
+    form.dirtyNavigationListener = e => {
+        if(form.dirty){
+            e.preventDefault();
+            e.returnValue = '';
+        }
+    }
+    window.addEventListener('beforeunload', form.dirtyNavigationListener)
 }
 
-function preventDirtyNavigation(){
-    if(dirtyNavInited || dirtyNavDisabled) return
-    dirtyNavInited = true
-    window.addEventListener('beforeunload', dirtyNavigationListener)
-}
 
-function dirtyNavigationListener(event){
-    if(dirtyFields.size !== 0){
-        event.preventDefault();
-        event.returnValue = '';
+// Standardize field data representation (helper)
+function fieldState(field){
+    let changed = false
+    let value = null
+    switch(field.type){
+        case 'radio':
+            return {
+                changed: field.defaultChecked !== field.checked,
+                value: field.checked? field.value : null
+            }
+        break;
+        case 'checkbox':
+            if(field.closest('form')[field.name].length){ // multiple
+                value = []
+                for(let checkbox of field.closest('form')[field.name]){
+                    if(checkbox.defaultChecked !== checkbox.checked) changed = true
+                    if(checkbox.checked) value.push(checkbox.value)
+                }
+            }else{ // single
+                value = field.checked? (field.value === "true"? true : field.value) : (field.value === "true"? false : null)
+                changed = field.defaultChecked !== field.checked
+            }
+            return {
+                changed,
+                value
+            }
+        break;
+        case 'select-multiple':
+            value = []
+            for(let option of field.options){
+                if(option.defaultSelected !== option.selected) changed = true
+                if(option.selected) value.push(option.value)
+            }            
+            return {
+                changed,
+                value
+            }
+        break;
+        case 'select-one':
+            for(let option of field.options){
+                if(option.defaultSelected !== option.selected){
+                    changed = true
+                }
+            }
+            return {
+                changed,
+                value: field.value
+            }
+        break;
+        default:
+            return {
+                changed: field.defaultValue !== field.value,
+                value: field.value
+            }
+        break;
     }
 }
 
-// Objectify Form
+
+// Convert form fields to payload object 
 export function objectifyForm(form){
-    const obj = {}
-    for(let elem of form.elements){
-        switch(elem.type){
-            case 'radio':
-                if(!obj[elem.name]){
-                    obj[elem.name] = elem.checked? elem.value : null
-                }                
-            break;
-            case 'checkbox':
-                if(form[elem.name].length){
-                    if(!obj[elem.name]?.push){
-                        obj[elem.name] = []
-                    }
-                    if(elem.checked){
-                        obj[elem.name].push(elem.value)
-                    }
-                }else{
-                    if(elem.checked){
-                        obj[elem.name] = elem.value
-                    } 
-                }
-            break;
-            case 'select-multiple':
-                if(!obj[elem.name]?.push){
-                    obj[elem.name] = []
-                }
-                for(let option of elem.options){
-                    if(option.selected){
-                        obj[elem.name].push(option.value)
-                    }
-                }
-            break;
-            case 'submit':
-                // don't capture it
-            break;
-            case 'fieldset':
-                // don't capture it
-            break;
-            default:
-                obj[elem.name] = elem.value? elem.value : null
-            break;
-        }        
+    const body = {}
+    for(let field of form.elements){
+        if(['submit', 'fieldset'].includes(field.type)) continue;
+        body[field.name] = fieldState(field).value
     }
-
-    return obj
+    return body
 }
