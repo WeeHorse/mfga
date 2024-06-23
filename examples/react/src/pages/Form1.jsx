22f7f2881a8e1a50ff7ac17226905a1dda6bdfbe
Benjamin Berglund <benjaminberglund@gmail.com>
2024-06-23 12:05:21 +0200

    updated react examples

--- examples/react/src/pages/Form1.jsx
+++ examples/react/src/pages/Form1.jsx
@@ -3,54 +3,54 @@ import { watchFormState, handleSubmit } from "../../../../index.js"
 export default function () {
 
     return (
-    <form id="form1" onSubmit={handleSubmit} onChange={watchFormState} method="post" action="https://api.restful-api.dev/objects">        
-        <h2>Basic form fields</h2>        
+        <form id="form1" onSubmit={handleSubmit} onChange={watchFormState} method="post" action="https://api.restful-api.dev/objects">
+            <h2>Basic form fields</h2>
 
-        <label htmlFor="name">Name</label>
-        <input name="name"/>
-        
-        <label htmlFor="slogan">Slogan</label>
-        <input name="slogan"/>
+            <label htmlFor="name">Name</label>
+            <input name="name" />
 
-        <label htmlFor="foo-or-bar">Foo or bar</label>
-        <select name="foo-or-bar">
-            <option>No option</option>
-            <option value="foo" selected>Foo</option>
-            <option value="bar">Bar</option>
-        </select>
+            <label htmlFor="slogan">Slogan</label>
+            <input name="slogan" />
 
-        <label htmlFor="pigs-in-a-blanket">Pigs in a blanket</label>
-        <select name="pigs-in-a-blanket" multiple>
-            <option>No option</option>
-            <option value="pigs">Pigs</option>
-            <option value="in">In</option>
-            <option value="a">A</option>
-            <option value="blanket">Blanket</option>
-        </select>
+            <label htmlFor="foo-or-bar">Foo or bar</label>
+            <select name="foo-or-bar" defaultValue="foo">
+                <option>No option</option>
+                <option value="foo">Foo</option>
+                <option value="bar">Bar</option>
+            </select>
 
-        <label htmlFor="no-selected-option">No selected option</label>
-        <select name="no-selected-option">
-            <option>First option is default</option>
-            <option value="one">One</option>
-            <option value="two">Two</option>
-        </select>
+            <label htmlFor="pigs-in-a-blanket">Pigs in a blanket</label>
+            <select name="pigs-in-a-blanket" multiple>
+                <option>No option</option>
+                <option value="pigs">Pigs</option>
+                <option value="in">In</option>
+                <option value="a">A</option>
+                <option value="blanket">Blanket</option>
+            </select>
 
-        <label htmlFor="multiple-pre-selected-options">Multiple pre-selected options</label>
-        <select name="multiple-pre-selected-options" multiple>
-            <option value="pigs" selected>Pigs</option>
-            <option value="in" selected>In</option>
-            <option value="a">A</option>
-            <option value="blanket">Blanket</option>
-        </select>
+            <label htmlFor="no-selected-option">No selected option</label>
+            <select name="no-selected-option">
+                <option>First option is default</option>
+                <option value="one">One</option>
+                <option value="two">Two</option>
+            </select>
 
-        <p>There form has a hidden field with the name <em>"id"</em> and the value <em>"11"</em></p>
-        <input type="hidden" name="id" value="11"/>
+            <label htmlFor="multiple-pre-selected-options">Multiple pre-selected options</label>
+            <select name="multiple-pre-selected-options" multiple defaultValue={['pigs', 'in']}>
+                <option value="pigs">Pigs</option>
+                <option value="in">In</option>
+                <option value="a">A</option>
+                <option value="blanket">Blanket</option>
+            </select>
 
-        <label><input type="checkbox" name="robot" value="true" checked/>I am a Robot</label>
+            <p>There form has a hidden field with the name <em>"id"</em> and the value <em>"11"</em></p>
+            <input type="hidden" name="id" value="11" />
 
-        <input type="submit" value="submit" disabled/>
-        
-        <p><em>Inspect the payload in the Network tab in Developer Tools</em></p>
-    </form>
+            <label><input type="checkbox" name="robot" value="true" defaultChecked />I am a Robot</label>
+
+            <input type="submit" value="submit" disabled />
+
+            <p><em>Inspect the payload in the Network tab in Developer Tools</em></p>
+        </form>
     )
 }
--- examples/react/src/pages/Form4.jsx
+++ examples/react/src/pages/Form4.jsx
@@ -3,15 +3,15 @@ import { watchFormState, handleSubmit } from "../../../../index.js"
 export default function () {
 
     return (
-    <form enctype="multipart/form-data" id="form4" onSubmit={handleSubmit} onChange={watchFormState} method="post" action="https://api.restful-api.dev/objects">
-        <h2>Form 4</h2>
-        <input name="name" value="No name"/>
-        <input name="slogan" value ="No slogan"/>
-        <label><input type="file" name="files" multiple/>Select files</label>  
-        <input type="reset" value="reset"/>
-        <input type="submit" value="submit" disabled/>
-      
-        <p><em>Inspect the payload in the Network tab in Developer Tools</em></p>
-    </form>
+        <form encType="multipart/form-data" id="form4" onSubmit={handleSubmit} onChange={watchFormState} method="post" action="https://api.restful-api.dev/objects">
+            <h2>Form 4</h2>
+            <input name="name" defaultValue="No name" />
+            <input name="slogan" defaultValue="No slogan" />
+            <label><input type="file" name="files" multiple />Select files</label>
+            <input type="reset" value="reset" />
+            <input type="submit" value="submit" disabled />
+
+            <p><em>Inspect the payload in the Network tab in Developer Tools</em></p>
+        </form>
     )
 }
